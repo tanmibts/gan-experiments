b73bd6c670f076a8fad76b8c4a9710a1f9686772
tanmibts <victoria.em1281@gmail.com>
2018-10-28 21:27:31 -0400

    implemented variable scoping, some tensorboard (histograms,graph)

--- gan.py
+++ gan.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+from mlxtend.data import loadlocal_mnist
 
 
 class GAN:
@@ -17,7 +18,7 @@ class GAN:
         self.train_writer = None
 
         self.iterator = None
-        self.prep_data(raw_images_data)
+        self._prep_data(raw_images_data)
 
         # Placeholder for input images to the discriminator
         self.x_placeholder = tf.placeholder(
@@ -26,7 +27,7 @@ class GAN:
         self.z_placeholder = tf.placeholder(
             tf.float32, [None, self.Z_DIM])
 
-    def prep_data(self, raw_images_data):
+    def _prep_data(self, raw_images_data):
         # TODO: IMPLEMENT THIS WITH A NEW DATA SET
         dataset = tf.data.Dataset.from_tensor_slices(raw_images_data)
         self.iterator = dataset.repeat().batch(self.BATCH_SIZE).make_one_shot_iterator()
@@ -34,37 +35,40 @@ class GAN:
         # TODO: IMPLEMENT CYCLEGAN?
 
     def train(self):
+        # TensorBoard
+        self.merged = tf.summary.merge_all()
+
         # retrieve graph
         g_loss, d_loss = self._get_losses()
         trainerD, trainerG = self._get_trainers(g_loss, d_loss)
 
         # initialize graph
-        sess = tf.Session()
+        with tf.Session() as sess:
+            # TensorBoard
+            self.train_writer = tf.summary.FileWriter('tensorboard/train',
+                                                      sess.graph)
+            sess.run(tf.global_variables_initializer())
+            next_el = self.iterator.get_next()
+            for i in range(self.TRAIN_ITERS):
+                # generate inputs
+                z_batch = np.random.uniform(-1, 1,
+                                            size=[self.BATCH_SIZE, self.Z_DIM])
+                real_image_batch = sess.run(next_el)
+                real_image_batch = np.reshape(
+                    real_image_batch, [self.BATCH_SIZE, self.X_DIM, self.X_DIM, 1])
 
-        # TensorBoard
-        self.merged = tf.summary.merge_all()
-        self.train_writer = tf.summary.FileWriter('tensorboard/train',
-                                                  sess.graph)
-        sess.run(tf.global_variables_initializer())
-        next_el = self.iterator.get_next()
-        for i in range(self.TRAIN_ITERS):
-            # generate inputs
-            z_batch = np.random.uniform(-1, 1,
-                                        size=[self.BATCH_SIZE, self.Z_DIM])
-            real_image_batch = sess.run(next_el)
-            real_image_batch = np.reshape(
-                real_image_batch, [self.BATCH_SIZE, self.X_DIM, self.X_DIM, 1])
+                # run through net
+                _, dLoss, summary = sess.run([trainerD, d_loss, self.merged], feed_dict={
+                    self.z_placeholder: z_batch, self.x_placeholder: real_image_batch})  # Update the discriminator
+                _, gLoss = sess.run([trainerG, g_loss], feed_dict={
+                                    self.z_placeholder: z_batch})  # Update the generator
+                self.train_writer.add_summary(summary, i)
 
-            # run through net
-            _, dLoss, summary = sess.run([trainerD, d_loss, self.merged], feed_dict={
-                self.z_placeholder: z_batch, self.x_placeholder: real_image_batch})  # Update the discriminator
-            _, gLoss = sess.run([trainerG, g_loss], feed_dict={
-                                self.z_placeholder: z_batch})  # Update the generator
-            self.train_writer.add_summary(summary, 1)
-
-            # print update
-            if i % 200 == 0:
-                print('dLoss: %f\ngLoss: %f\n' % (dLoss, gLoss))
+                # print update
+                if i % 200 == 0:
+                    print('dLoss: %f\ngLoss: %f\n' % (dLoss, gLoss))
+                if i == 5:
+                    print('training going ok')
 
     def _get_losses(self):
         # Dx will hold discriminator prediction probabilities for the real MNIST images
@@ -92,151 +96,154 @@ class GAN:
         d_vars = [var for var in tvars if 'd_' in var.name]
         g_vars = [var for var in tvars if 'g_' in var.name]
 
-        trainerD = self.optimizer.minimize(d_loss, var_list=d_vars)
-        trainerG = self.optimizer.minimize(g_loss, var_list=g_vars)
+        with tf.variable_scope('d_train'):
+            trainerD = self.optimizer.minimize(d_loss, var_list=d_vars)
+        with tf.variable_scope('g_train'):
+            trainerG = self.optimizer.minimize(g_loss, var_list=g_vars)
         return trainerD, trainerG
 
-    def _conv2d(x, W):
-        return tf.nn.conv2d(input=x, filter=W, strides=[1, 1, 1, 1], padding='SAME')
-
-    def _avg_pool_2x2(x):
-        return tf.nn.avg_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-
-    def variable_summaries(var):
+    def _variable_summaries(var):
         """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
         with tf.name_scope('summaries'):
-            mean = tf.reduce_mean(var)
-            tf.summary.scalar('mean', mean)
-            with tf.name_scope('stddev'):
-                stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-            tf.summary.scalar('stddev', stddev)
-            tf.summary.scalar('max', tf.reduce_max(var))
-            tf.summary.scalar('min', tf.reduce_min(var))
+            # mean = tf.reduce_mean(var)
+            # tf.summary.scalar('mean', mean)
+            # with tf.name_scope('stddev'):
+            #     stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+            # tf.summary.scalar('stddev', stddev)
+            # tf.summary.scalar('max', tf.reduce_max(var))
+            # tf.summary.scalar('min', tf.reduce_min(var))
             tf.summary.histogram('histogram', var)
+
+    def _d_conv_layer(layer, filter):
+        W_conv = tf.get_variable(
+            'd_weights_conv', filter, initializer=tf.truncated_normal_initializer(stddev=.01))
+        b_conv = tf.get_variable(
+            'd_bias_conv', [filter[-1]], initializer=tf.constant_initializer(0))
+
+        GAN._variable_summaries(W_conv)
+        GAN._variable_summaries(b_conv)
+
+        layer = tf.nn.conv2d(input=layer, filter=W_conv, strides=[
+                             1, 1, 1, 1], padding='SAME')
+        layer = tf.nn.relu(layer + b_conv)
+        layer = tf.nn.avg_pool(layer, ksize=[1, 2, 2, 1], strides=[
+                               1, 2, 2, 1], padding='SAME')
+        return layer
+
+    def _d_fc_layer(layer, in_dim, out_dim, last):
+        W_fc = tf.get_variable(
+            'd_weights_fc', [in_dim, out_dim], initializer=tf.truncated_normal_initializer(stddev=.01))
+        b_fc = tf.get_variable(
+            'b_fc', [out_dim], initializer=tf.constant_initializer(0))
+        layer = tf.matmul(layer, W_fc) + b_fc
+        if not last:
+            layer = tf.nn.relu(layer)
+        return layer
 
     def _discriminator(self, x_image, reuse=False):
         with tf.variable_scope('discriminator') as scope:
             if (reuse):
-                tf.get_variable_scope().reuse_variables()
+                scope.reuse_variables()
+
             # First Conv and Pool Layers
-            W_conv1 = tf.get_variable(
-                'd_wconv1', [5, 5, 1, 8], initializer=tf.truncated_normal_initializer(stddev=0.02))
-            b_conv1 = tf.get_variable(
-                'd_bconv1', [8], initializer=tf.constant_initializer(0))
-            GAN.variable_summaries(W_conv1)
-            GAN.variable_summaries(b_conv1)
-            h_conv1 = tf.nn.relu(GAN._conv2d(x_image, W_conv1) + b_conv1)
-            h_pool1 = GAN._avg_pool_2x2(h_conv1)
+            with tf.variable_scope('conv1'):
+                layer = GAN._d_conv_layer(x_image, [5, 5, 1, 8])
 
             # Second Conv and Pool Layers
-            W_conv2 = tf.get_variable('d_wconv2', [
-                                      5, 5, 8, 16], initializer=tf.truncated_normal_initializer(stddev=0.02))
-            b_conv2 = tf.get_variable(
-                'd_bconv2', [16], initializer=tf.constant_initializer(0))
-            h_conv2 = tf.nn.relu(GAN._conv2d(h_pool1, W_conv2) + b_conv2)
-            h_pool2 = GAN._avg_pool_2x2(h_conv2)
+            with tf.variable_scope('conv2'):
+                layer = GAN._d_conv_layer(layer, [5, 5, 8, 16])
 
             # First Fully Connected Layer
-            W_fc1 = tf.get_variable('d_wfc1', [
-                                    7 * 7 * 16, 32], initializer=tf.truncated_normal_initializer(stddev=0.02))
-            b_fc1 = tf.get_variable(
-                'd_bfc1', [32], initializer=tf.constant_initializer(0))
-            h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 16])
-            h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+            layer = tf.reshape(layer, [-1, 7 * 7 * 16])
+            with tf.variable_scope('fc1'):
+                layer = GAN._d_fc_layer(layer, 7 * 7 * 16, 32, False)
 
-            # Second Fully Connected Layer
-            W_fc2 = tf.get_variable(
-                'd_wfc2', [32, 1], initializer=tf.truncated_normal_initializer(stddev=0.02))
-            b_fc2 = tf.get_variable(
-                'd_bfc2', [1], initializer=tf.constant_initializer(0))
-
-            # Final Layer
-            y_conv = (tf.matmul(h_fc1, W_fc2) + b_fc2)
+            # Second (Final) Fully Connected Layer
+            with tf.variable_scope('fc2'):
+                y_conv = GAN._d_fc_layer(layer, 32, 1, True)
 
         return y_conv
+
+    def _g_deconv_layer(layer, output_shape, scope):
+        W_conv = tf.get_variable('g_weights_conv',
+                                 [5, 5, output_shape[-1],
+                                  int(layer.get_shape()[-1])],
+                                 initializer=tf.truncated_normal_initializer(stddev=0.1))
+        b_conv = tf.get_variable('g_bias_conv',
+                                 [output_shape[-1]],
+                                 initializer=tf.constant_initializer(.1))
+
+        GAN._variable_summaries(W_conv)
+        GAN._variable_summaries(b_conv)
+
+        layer = tf.nn.conv2d_transpose(layer, W_conv, output_shape=output_shape,
+                                       strides=[1, 2, 2, 1], padding='SAME') + b_conv
+        layer = tf.contrib.layers.batch_norm(
+            inputs=layer, center=True, scale=True, is_training=True, scope=scope)
+        layer = tf.nn.relu(layer)
+        return layer
 
     def _generator(self, z, batch_size, z_dim, reuse=False):
         with tf.variable_scope('generator') as scope:
             if (reuse):
-                tf.get_variable_scope().reuse_variables()
+                scope.reuse_variables()
             g_dim = 64  # Number of filters of first layer of generator
-            # Color dimension of output (MNIST is grayscale, so c_dim = 1 for us)
-            c_dim = 1
-            s = 28  # Output size of the image
-            # We want to slowly upscale the image, so these values will help
+            c_dim = 1  # 1 color channel in output image
+            s = 28  # dimension of output image
             s2, s4, s8, s16 = int(s / 2), int(s / 4), int(s / 8), int(s / 16)
-            # make that change gradual.
 
-            h0 = tf.reshape(z, [batch_size, s16 + 1, s16 + 1, 25])
-            h0 = tf.nn.relu(h0)
-            # Dimensions of h0 = batch_size x 2 x 2 x 25
+            layer = tf.reshape(z, [batch_size, s16 + 1, s16 + 1, 25])
+            layer = tf.nn.relu(layer)
+            # Dimensions = batch_size x 2 x 2 x 25
 
             # First DeConv Layer
             output1_shape = [batch_size, s8, s8, g_dim * 4]
-            W_conv1 = tf.get_variable('g_wconv1', [5, 5, output1_shape[-1], int(h0.get_shape()[-1])],
-                                      initializer=tf.truncated_normal_initializer(stddev=0.1))
-            b_conv1 = tf.get_variable(
-                'g_bconv1', [output1_shape[-1]], initializer=tf.constant_initializer(.1))
-            H_conv1 = tf.nn.conv2d_transpose(h0, W_conv1, output_shape=output1_shape,
-                                             strides=[1, 2, 2, 1], padding='SAME') + b_conv1
-            H_conv1 = tf.contrib.layers.batch_norm(
-                inputs=H_conv1, center=True, scale=True, is_training=True, scope="g_bn1")
-            H_conv1 = tf.nn.relu(H_conv1)
-            # Dimensions of H_conv1 = batch_size x 3 x 3 x 256
+            with tf.variable_scope('conv1'):
+                layer = GAN._g_deconv_layer(
+                    layer, output1_shape, 'g_bn1')
+            # Dimensions = batch_size x 3 x 3 x 256
 
             # Second DeConv Layer
             output2_shape = [batch_size, s4 - 1, s4 - 1, g_dim * 2]
-            W_conv2 = tf.get_variable('g_wconv2', [5, 5, output2_shape[-1], int(H_conv1.get_shape()[-1])],
-                                      initializer=tf.truncated_normal_initializer(stddev=0.1))
-            b_conv2 = tf.get_variable(
-                'g_bconv2', [output2_shape[-1]], initializer=tf.constant_initializer(.1))
-            H_conv2 = tf.nn.conv2d_transpose(H_conv1, W_conv2, output_shape=output2_shape,
-                                             strides=[1, 2, 2, 1], padding='SAME') + b_conv2
-            H_conv2 = tf.contrib.layers.batch_norm(
-                inputs=H_conv2, center=True, scale=True, is_training=True, scope="g_bn2")
-            H_conv2 = tf.nn.relu(H_conv2)
-            # Dimensions of H_conv2 = batch_size x 6 x 6 x 128
+            with tf.variable_scope('conv2'):
+                layer = GAN._g_deconv_layer(
+                    layer, output2_shape, 'g_bn2')
+            # Dimensions = batch_size x 6 x 6 x 128
 
             # Third DeConv Layer
             output3_shape = [batch_size, s2 - 2, s2 - 2, g_dim * 1]
-            W_conv3 = tf.get_variable('g_wconv3', [5, 5, output3_shape[-1], int(H_conv2.get_shape()[-1])],
-                                      initializer=tf.truncated_normal_initializer(stddev=0.1))
-            b_conv3 = tf.get_variable(
-                'g_bconv3', [output3_shape[-1]], initializer=tf.constant_initializer(.1))
-            H_conv3 = tf.nn.conv2d_transpose(H_conv2, W_conv3, output_shape=output3_shape,
-                                             strides=[1, 2, 2, 1], padding='SAME') + b_conv3
-            H_conv3 = tf.contrib.layers.batch_norm(
-                inputs=H_conv3, center=True, scale=True, is_training=True, scope="g_bn3")
-            H_conv3 = tf.nn.relu(H_conv3)
-            # Dimensions of H_conv3 = batch_size x 12 x 12 x 64
+            with tf.variable_scope('conv3'):
+                layer = GAN._g_deconv_layer(
+                    layer, output3_shape, 'g_bn3')
+            # Dimensions = batch_size x 12 x 12 x 64
 
             # Fourth DeConv Layer
             output4_shape = [batch_size, s, s, c_dim]
-            W_conv4 = tf.get_variable('g_wconv4', [5, 5, output4_shape[-1], int(H_conv3.get_shape()[-1])],
-                                      initializer=tf.truncated_normal_initializer(stddev=0.1))
-            b_conv4 = tf.get_variable(
-                'g_bconv4', [output4_shape[-1]], initializer=tf.constant_initializer(.1))
-            H_conv4 = tf.nn.conv2d_transpose(H_conv3, W_conv4, output_shape=output4_shape,
-                                             strides=[1, 2, 2, 1], padding='VALID') + b_conv4
-            H_conv4 = tf.nn.tanh(H_conv4)
-            # Dimensions of H_conv4 = batch_size x 28 x 28 x 1
+            with tf.variable_scope('conv4'):
+                W_conv4 = tf.get_variable('g_weights_conv',
+                                          [5, 5, output4_shape[-1],
+                                           int(layer.get_shape()[-1])],
+                                          initializer=tf.truncated_normal_initializer(stddev=0.1))
+                b_conv4 = tf.get_variable('g_bias_conv',
+                                          [output4_shape[-1]],
+                                          initializer=tf.constant_initializer(.1))
+                layer = tf.nn.conv2d_transpose(layer, W_conv4, output_shape=output4_shape,
+                                               strides=[1, 2, 2, 1], padding='VALID') + b_conv4
+                layer = tf.nn.tanh(layer)
+            # Dimensions = batch_size x 28 x 28 x 1
 
-        return H_conv4
-
-
-def load_local_mnist(images_path, labels_path):
-
-    return images, labels
+        return layer
 
 
 if __name__ == "__main__":
     # import data
     # from tensorflow.examples.tutorials.mnist import input_data
     # mnist = input_data.read_data_sets("MNIST_data/")
-    from mlxtend.data import loadlocal_mnist
+
     train_images, _ = loadlocal_mnist(
         'MNIST_data/t10k-images-idx3-ubyte', 'MNIST_data/t10k-labels-idx1-ubyte')
     print('SHAPE IS ', train_images.shape)
+
     # run model
     model = GAN(train_images)
     model.train()
